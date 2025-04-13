44171f131158f859f5d1d5dda1315ba3ddb11004
ZmolZito <maclucas07@hotmail.com>
2025-04-13 23:51:14 +0200

    ResponseDto<T> added subscriptionservice

--- SubscriptionService/SubscriptionService.Application/Queries/Interfaces/IForumSubQuery.cs
+++ SubscriptionService/SubscriptionService.Application/Queries/Interfaces/IForumSubQuery.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SubscriptionService.Application.Dto;
 using SubscriptionService.Application.Queries.QueryDto;
 
 namespace SubscriptionService.Application.Queries.Interfaces
