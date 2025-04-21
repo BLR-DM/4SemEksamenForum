bdfbab8e4493391db1a58b7fc3be6f2671072f71
skovbov <skovbov@gmail.com>
2025-04-21 22:09:58 +0200

    pubsub virker

--- VoteService/VoteService.Application/Commands/CommentVoteCommand.cs
+++ VoteService/VoteService.Application/Commands/CommentVoteCommand.cs
@@ -10,31 +10,6 @@ namespace VoteService.Application.Commands;
 
 public class CommentVoteCommand : ICommentVoteCommand
 {
-    //private readonly ICommentVoteRepository _repository;
-
-    //public CommentVoteCommand(ICommentVoteRepository repository)
-    //{
-    //    _repository = repository;
-    //}
-    //async Task ICommentVoteCommand.ToggleCommentVote(string commentId, CommentVoteDto dto)
-    //{
-    //    var existingVote = await _repository.GetVoteByUserIdAsync(dto.UserId, commentId);
-
-    //    if (existingVote == null)
-    //    {
-    //        var newVote = CommentVote.Create(dto.UserId, commentId, dto.VoteType);
-    //        await _repository.AddCommentVoteAsync(newVote);
-    //    }
-    //    else if (existingVote.VoteType == dto.VoteType)
-    //    {
-    //        await _repository.DeleteCommentVoteAsync(existingVote);
-    //    }
-    //    else
-    //    {
-    //        existingVote.Update(dto.VoteType);
-    //        await _repository.UpdateVoteAsync(existingVote);
-    //    }
-    //}
     private readonly CommentVoteService _commentVoteService;
     private readonly IEventHandler _eventHandler;
 
--- VoteService/VoteService.Application/Commands/PostVoteCommand.cs
+++ VoteService/VoteService.Application/Commands/PostVoteCommand.cs
@@ -10,32 +10,6 @@ namespace VoteService.Application.Commands;
 
 public class PostVoteCommand : IPostVoteCommand
 {
-    //private readonly IPostVoteRepository _repository;
-
-    //public PostVoteCommand(IPostVoteRepository repository)
-    //{
-    //    _repository = repository;
-    //}
-
-    //async Task IPostVoteCommand.TogglePostVote(string postId, PostVoteDto dto)
-    //{
-    //    var existingVote = await _repository.GetVoteByUserIdAsync(dto.UserId, postId);
-
-    //    if (existingVote == null)
-    //    {
-    //        var newVote = PostVote.Create(dto.UserId, postId, dto.VoteType);
-    //        await _repository.AddPostVoteAsync(newVote);
-    //    }
-    //    else if (existingVote.VoteType == dto.VoteType)
-    //    {
-    //        await _repository.DeletePostVoteAsync(existingVote);
-    //    }
-    //    else
-    //    {
-    //        existingVote.Update(dto.VoteType);
-    //        await _repository.UpdateVoteAsync(existingVote);
-    //    }
-    //}
     private readonly PostVoteService _postVoteService;
     private readonly IEventHandler _eventHandler;
 
