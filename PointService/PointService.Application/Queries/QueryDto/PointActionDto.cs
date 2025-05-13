5e9079224bef71948015cb34c63f1944cc0ef0cf
Lucas <LucasOMQ@Hotmail.com>
2025-05-13 23:19:57 +0200

    Added PointActionQuery

---
+++ PointService/PointService.Application/Queries/QueryDto/PointActionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointService.Application.Queries.QueryDto
+{
+    public class PointActionDto
+    {
+        public string Action { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ PointService/PointService.Infrastructure/Queries/PointActionQuery.cs
@@ -0,0 +1,37 @@
+﻿using PointService.Application.Queries.QueryDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PointService.Infrastructure.Queries
+{
+    public class PointActionQuery : IPointActionQuery
+    {
+        private readonly PointContext _db;
+        public PointActionQuery(PointContext db)
+        {
+            _db = db;
+        }
+
+        async Task<List<PointActionDto>> IPointActionQuery.GetAllPointActionsAsync()
+        { 
+            var pointActions = await _db.PointActions.ToListAsync();
+
+            var pointActionDtos = pointActions.Select(pa => new PointActionDto
+            {
+                Action = pa.Action,
+                Points = pa.Points
+            }).ToList();
+
+            return pointActionDtos;
+        }
+    }
+
+    public interface IPointActionQuery
+    {
+        Task<List<PointActionDto>> GetAllPointActionsAsync();
+    }
+}
