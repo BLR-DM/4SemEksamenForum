118bd20a020868016e0f7fb4e03b7c629fda78c6
Bilal Kinali <biki59986@edu.ucl.dk>
2025-05-19 02:17:24 +0200

    Added tests for Forum domain

---
+++ ContentService/ContentService.Domain.Test/ForumTests.cs
@@ -0,0 +1,49 @@
+﻿using ContentService.Domain.Entities;
+
+namespace ContentService.Domain.Test
+{
+    public class ForumTests
+    {
+        [Fact]
+        public void CreateForum__WhenSpacesInName_Throws()
+        {
+            var otherForums = new List<Forum>();
+
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                Forum.Create("Test Forum", "Test content", "userId1", otherForums));
+
+            Assert.Equal("Forum name cannot have spaces", ex.Message);
+        }
+
+        [Fact]
+        public void CreateForum__WhenNameIsNotUnique_Throws()
+        {
+            var otherForums = new List<Forum>
+            {
+                Forum.Create("NameIsNotUnique", "Test content", "userId1", [])
+            };
+
+            var ex = Assert.Throws<InvalidOperationException>(() =>
+                Forum.Create("nameisnotunique", "Test content", "userId2", otherForums));
+
+            Assert.Equal("Forum name already exists", ex.Message);
+        }
+
+        [Fact]
+        public void CreateForum__WhenNoSpacesInName_Creates()
+        {
+            Assert.IsType<Forum>(Forum.Create("NoSpacesInName", "Random content", "userId1", []));
+        }
+
+        [Fact]
+        public void CreateForum__WhenForumNameIsUnique_Creates()
+        {
+            var otherForums = new List<Forum>
+            {
+                Forum.Create("UniqueForum", "Random content", "userId1", [])
+            };
+
+            Assert.IsType<Forum>(Forum.Create("NoSpacesInName", "Random content", "userId2", otherForums));
+        }
+    }
+}
